2998214d7ecc9d0fb5e4abea5cd3c1c8b0306575
Alton1998 <39479720+Alton1998@users.noreply.github.com>
2019-12-01 20:40:16 +0530

    Add files via upload

--- src/app/order-history/order-history.page.ts
+++ src/app/order-history/order-history.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-order-history',
@@ -6,10 +10,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-history.page.scss'],
 })
 export class OrderHistoryPage implements OnInit {
-
-  constructor() { }
-
+  orderRef:AngularFireList<any>
+  orders:Observable<any>
+  orderItem:Array<any>
+  constructor(private db:AngularFireDatabase,private auth:AngularFireAuth,private router:Router) {
+   }
+   getorder()
+   {
+     this.orderRef=this.db.list('/users/'+this.auth.auth.currentUser.uid+'/orders/')
+     this.orders=this.orderRef.snapshotChanges()
+     this.orders.subscribe((res)=>
+     {
+       this.orderItem=[]
+       res.forEach((action)=>
+       {
+         let dict={}
+         dict['products']=action.payload.val()
+         dict['key']=action.payload.key
+         console.log(dict)
+         this.orderItem.push(dict)
+       })
+     })
+   }
   ngOnInit() {
+    console.log(this.orderItem)
   }
-
+  ionViewDidEnter()
+  {
+    this.getorder()
+  }
+  repeatOrder(order)
+  {
+    console.log(order)
+    this.router.navigateByUrl('/order-summary',{state:{products:order}})
+  }
+  
 }
